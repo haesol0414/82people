8cd33159be0fd7848add7b3a8816314bddf6e161
Haesol <hsler404@gmail.com>
2023-08-01 01:14:35 +0900

    08.01 베스트 상품 단일 슬라이드 구현

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,7 +2,31 @@ import { main } from '/Common/index.js';
 await main();
 
 const total = document.querySelector('.products-total');
-const best = document.querySelector('.products-best');
+let currentIdx = 0; //현재 슬라이드 index
+const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
+const slideCount = 3; // 슬라이드 개수
+const prev = document.querySelector('.prev'); //이전 버튼
+const next = document.querySelector('.next'); //다음 버튼
+const slideWidth = 500; //한개의 슬라이드 넓이
+const slideMargin = 100; //슬라이드간의 margin 값
+
+//전체 슬라이드 컨테이너 넓이 설정
+slides.style.width = (slideWidth + slideMargin) * (slideCount + 1) + 'px';
+
+function moveSlide(num) {
+	slides.style.left = -num * (slideWidth + slideMargin) + 'px';
+	currentIdx = num;
+}
+
+prev.addEventListener('click', function () {
+	if (currentIdx !== 0) moveSlide(currentIdx - 1);
+});
+
+next.addEventListener('click', function () {
+	if (currentIdx !== slideCount - 1) {
+		moveSlide(currentIdx + 1);
+	}
+});
 
 fetch('/api/products', {
 	method: 'GET',
@@ -30,21 +54,6 @@ fetch('/api/products', {
 	})
 	.catch(err => console.log(err));
 
-let bestItems = '';
-const getBestProducts = bestProduct => {
-	const newBestItem = `<li>
-		<a class='product-link'
-		href='/products?productId=${bestProduct._id}' target='_self'>
-		<img class="product-img"
-		src='${bestProduct.imageURL}' alt="product-item"/>
-		</div>
-		</a>
-    </li>`;
-
-	bestItems += newBestItem;
-	best.innerHTML = bestItems;
-};
-
 let items = '';
 const getTotalProducts = newProduct => {
 	const newItem = `<li>
@@ -59,4 +68,16 @@ const getTotalProducts = newProduct => {
     </li>`;
 	items += newItem;
 	total.innerHTML = items;
+};
+
+let bestItems = '';
+const getBestProducts = bestProduct => {
+	const newBestItem = `
+		<li>
+		<a class='product-link'
+		href='/products?productId=${bestProduct._id}' target='_self'>
+		<img src="${bestProduct.imageURL}" /></li>
+	`;
+	bestItems += newBestItem;
+	slides.innerHTML = bestItems;
 };
