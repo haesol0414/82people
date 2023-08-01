38672859fad1daa4548aec795ddcd992a78b7988
Haesol <hsler404@gmail.com>
2023-08-02 04:55:18 +0900

    08.02 관리자 - 상품 삭제 API 생성

--- src/views/AdminItemManage/AdminItemManage.js
+++ src/views/AdminItemManage/AdminItemManage.js
@@ -3,6 +3,8 @@ await main();
 const hasToken = await ConfirmToken();
 
 const itemsList = document.querySelector('#table-body');
+const createBtn = document.querySelector('#createBtn');
+
 fetch('/api/products', {
 	method: 'GET',
 	headers: {
@@ -30,6 +32,7 @@ fetch('/api/products', {
 let items = '';
 const getItems = item => {
 	const newItem = `
+	<tr name="table-body" product_id="${item._id}">
 		<td><input type="radio"></td>
 		<td name="image">
 		<div class="img-box">
@@ -47,7 +50,58 @@ const getItems = item => {
 	itemsList.innerHTML = items;
 };
 
+// 상품 추가 버튼
+createBtn.addEventListener('click', () => {
+	location.href = `/admin/items/addItem`;
+});
+
+// 상품 삭제
+const deleteBtn = document.getElementById('deleteBtn');
+
+function deleteSelectedRows() {
+	const userConfirm = confirm('선택된 상품을 정말 삭제하시겠습니까?');
+	if (userConfirm) {
+		let radioes = document.querySelectorAll(
+			".product tbody input[type='radio']"
+		);
+		let rowsToDelete = [];
+
+		radioes.forEach(function (radio) {
+			if (radio.checked) {
+				rowsToDelete.push(radio.parentNode.parentNode);
+			}
+		});
+
+		rowsToDelete.forEach(function (row) {
+			row.parentNode.removeChild(row); // 화면에서 삭제
+			const id = row.getAttribute('product_id');
+			console.log(id);
+
+			fetch('/api/admin/items', {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: hasToken,
+				},
+				body: JSON.stringify({
+					productId: id,
+				}),
+			})
+				.then(res => res.json())
+				.catch(err => console.log(err))
+				.then(json => {
+					console.log(json);
+					// window.location.reload();
+				})
+				.catch(err => console.log(err));
+		}); // API에서 삭제
+	}
+}
+
+deleteBtn.addEventListener('click', deleteSelectedRows);
+
 window.onload = function () {
+	// 상품 수정 버튼
 	const modifyBtn = document.querySelectorAll('#modify-btn');
 
 	for (let i = 0; i < modifyBtn.length; i++) {
@@ -57,9 +111,3 @@ window.onload = function () {
 		});
 	}
 };
-
-const createBtn = document.querySelector('#createBtn');
-console.log(createBtn);
-createBtn.addEventListener('click', () => {
-	location.href = `/admin/items/addItem`;
-});
